705ad3bc7f1bc73fd80f0a41e61fc68da7aad09a
DmitriyB <demianbondarenko@gmail.com>
2019-06-05 11:16:13 +0300

    displaying get request

--- src/components/dashboard/dashboard.vue
+++ src/components/dashboard/dashboard.vue
@@ -2,8 +2,38 @@
   <div id="dashboard">
     <h1>That's the dashboard!</h1>
     <p>You should only get here if you're authenticated!</p>
+    <p>Your mail address: {{ email }}</p>
   </div>
 </template>
+
+<script>
+import axios from 'axios';
+export default {
+  data() {
+    return{
+      email: ''
+    }
+  },
+  
+  created(){
+    axios.get('https://vuejstest-72551.firebaseio.com/users.json')
+      .then(res => {
+        console.log(res)
+        const data = res.data
+        const users = []
+        for (let key in data){
+          const user = data[key]
+          user.id = key
+          users.push(user)
+        }
+        console.log(users)
+        this.email = users[2].email
+      })
+      .catch(error => console.log(error))
+  }
+}
+</script>
+
 
 <style scoped>
   h1, p {
